63106923975477631b36cba1b8d7465c91853b4f
nishalkdev <111446772+nishalkdev@users.noreply.github.com>
2022-09-21 12:01:37 -0400

    Add files via upload

---
+++ PowerShell/PnPSharePointCopyFiles.ps1
@@ -0,0 +1,20 @@
+﻿#Parameters
+$SiteURL = ""
+$SourceFolderURL = ""
+$TargetFolderURL = ""
+
+  
+#Connect to PnP Online
+Connect-PnPOnline -Url $SiteURL -Interactive
+  
+#Copy All Files and Folders between source and target folders
+Copy-PnPFile -SourceUrl $SourceFolderURL -TargetUrl $TargetFolderURL -Force -OverwriteIfAlreadyExists
+
+
+
+
+
+
+#notes
+
+#notes
